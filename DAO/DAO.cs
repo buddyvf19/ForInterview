bcf8c2fcd239e0ba6f3f9a22568b7bf08a6476f0
buddyvf19 <buddyvf19@gmail.com>
2020-09-11 20:25:18 +0800

    建立專案結構

---
+++ DAO/DAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DAO
+{
+    public class DAO
+    {
+    
+    }
+}
---
+++ ForInterView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ForInterView.MvcApplication" Language="C#" %>
